91cee78ae6308ac432143f0c7a8f7f6616e19934
Shema N. Valentin <shemavalentin@gmail.com>
2024-08-12 11:59:42 +0200

    Passed data saurce file in pipe and parse for readability

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const fs = require("fs");
 
 const result = []; // initializing an empty array to store the received data from the stream
 fs.createReadStream("kepler_data.csv")
+  //Here we really need to read data row by row as it come and in a readable way
+  // and this is done by parse func. how to connect parse with the function createReadStream?
+  // Because parse was designed to read streams, it uses another method to connect
+  // two interested stream together and read it like so:
+
+  // the pipe function is meant to connect a readable stream saurce to a
+  // writable stream destination(a stream that takes in data as supposed to giving you data)
+  // in this case the "kepler_data.csv" is the saurce while paser() is the destination for pipe()
+  .pipe(
+    parse({
+      // we can pass in object telling it how it will treate the saurce file
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
     // pushing data into the array
     result.push(data);
